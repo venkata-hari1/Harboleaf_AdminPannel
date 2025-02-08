c234849eabe32f2e2418e72aa4c7f65764801f4c
Sanjeev <sanjeev.ksv@gmail.com>
2025-02-08 13:57:47 +0530

    safety reports, edit profile

---
+++ src/Components/Safetyreports.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import '../Styles/Safetyreports.css';
+import safetyimg from "../assets/safetyimg.png"
+import Safetytable from './Safetytable';
+const Safetyreports = () => {
+
+    const safedata=[
+
+         {id:1,key:"Gender",value:"Female"},
+         {id:2,key:"Date of Birth", value:"12-02-1998"},
+         {id:3,key:"Phone number", value:"+91 7550723470"},
+         {id:4,key:"Rescued By", value:"Akhil pradhan"},
+         {id:5,key:"Emergency Time and Date", value:"11 : 53 AM, 13th October 2024"},
+         {id:6,key:"Emergency Location", value:"11 : 53 AM, 13th October 2024"}
+
+    ]
+
+  return (
+    <div className='container'>
+        <div className='safety-reports-box'>
+          <div className='img-box'> 
+           <img src={safetyimg} width="174px" height="213px" />
+          </div>  
+           <div className='safety-titles'>
+             <div className='safety-names'> 
+             <p>Navjot Kaur</p>
+             <p style={{color:"#FF0000"}}>[Critical Emergency]</p>
+             </div>
+             {
+              safedata.map(data=>(
+
+                 <div className='safety-data-box'>
+                   <p className='safety-key'> {(data.id===5 || data.id===6)?(<span style={{color:"#FF0000"}}>{data.key}</span>):(data.key)}</p>
+                   <p className='safety-value'>{data.id==4 && <img src="http://www.w3.org/2000/svg" className="rounded-circle" />}{data.value}</p>    
+          
+                 </div>
+
+
+              ))
+
+            }
+           </div>
+          
+           <div className='reportchat'>
+            <button className='download-chat'>Download Chat Report <i className="bi bi-download ms-2"></i></button>
+           </div>
+
+
+
+
+           </div>
+
+
+          <div>
+        </div>        
+          <Safetytable/>
+
+        </div>
+  
+  )
+}
+
+export default Safetyreports
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,6 +8,9 @@ import Profiledata from '../Components/Profiledata'
 import Socialdata from '../Components/Socialdata'
 import Postdata from '../Components/Postdata'
 import Profileinformation from '../Components/Profileinformation'
+import Editprofile from '../Components/Editprofile'
+import Safetyreports from '../Components/Safetyreports'
+import Safetytable from '../Components/Safetytable'
 const AppRoutes = () => {
   return (
     <Routes> 
@@ -19,7 +22,9 @@ const AppRoutes = () => {
       <Route path='/social' element={<Socialdata />}/> 
       <Route path='/post' element={<Postdata />}/> 
       <Route path='/profile-info' element={<Profileinformation />} />
-  
+      <Route path='/edit' element={<Editprofile />}/> 
+      <Route path='/safety-reports' element={<Safetyreports/>}/>
+      <Route path='/safetable' element={< Safetytable/>} />
     </Routes>
     
   )
