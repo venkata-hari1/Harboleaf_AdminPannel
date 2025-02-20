e2163c207c90fc72d28f8e2b514d3b337ab344c4
Sanjeev <sanjeev.ksv@gmail.com>
2025-02-20 15:02:10 +0530

    suspend buttons

--- src/Components/SuspendandFilter.jsx
+++ src/Components/SuspendandFilter.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SuspendandFilter = () => {
+
+  
+  const[suspens,setSuspend]=useState(null) 
+  const[dis,setDis]=useState("")
+  
+  function dropdownOpen(){
+    
+     setDis((prev)=>!prev)
+     setSuspend('action_btns_container')
+}
+
   return (
     <div>
       <div  style={{display:"flex",justifyContent:"end"}}>
-     <button className='btn btn-primary me-3' style={{backgroundColor:"#3856F3",fontFamily:"Roboto"}}>Filter <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-funnel-fill ms-1" viewBox="0 0 16 16">
+     <button  className='btn btn-primary me-3' style={{backgroundColor:"#3856F3",fontFamily:"Roboto"}}>Filter <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-funnel-fill ms-1" viewBox="0 0 16 16">
      <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/>
     </svg></button>
-      <button className='btn' style={{color:"#FF0000",border:"1px solid #FF0000",fontFamily:"Roboto"}}> Suspended Accounts</button>
+    
+      <button onClick={()=>dropdownOpen()} className='btn' style={{color:"#FF0000",border:"1px solid #FF0000",fontFamily:"Roboto"}}> Suspended Accounts</button>
+      <div className={suspens} style={{display:dis?"block":"none", marginTop:"40px"}}>
+            <button className='gst-retrive-child'>Temporary Suspended</button>
+            <button className='gst-retrive-child'>Suspended</button>
+            <button className='gst-retrive-child'>Deleted</button>
+        </div>
     </div>
 
     </div>
@@ -18,6 +35,7 @@ export default SuspendandFilter
 
 export const FilterandEmergency=()=>{
 
+  
 
     return(
         <div className='d-flex justify-content-end mt-4' style={{paddingLeft:"50px"}}>
@@ -32,7 +50,17 @@ export const FilterandEmergency=()=>{
 }
 
 export const FilterandSuspendAccount=()=>{
-   
+  
+  const[suspens,setSuspend]=useState(null) 
+  
+  const[dis,setDis]=useState("")
+  function dropdownOpen(){
+    
+    setDis((prev)=>!prev)
+    setSuspend('action_btns_container')
+
+
+  }
     return(
         <div>
           <div className='d-flex justify-content-end mt-4'>
@@ -40,7 +68,12 @@ export const FilterandSuspendAccount=()=>{
          <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/>
          </svg>
          </button>
-      <button className='btn' style={{color:"#FF0000",border:"1px solid #FF0000",fontFamily:"Roboto"}}>Suspended</button>
+      <button onClick={()=>dropdownOpen()}className='btn' style={{color:"#FF0000",border:"1px solid #FF0000",fontFamily:"Roboto"}}>Suspended</button>
+      <div className={suspens} style={{display:dis?"block":"none", marginTop:"40px"}}>
+            <button className='gst-retrive-child'>Temporary Suspended</button>
+            <button className='gst-retrive-child'>Suspended</button>
+            <button className='gst-retrive-child'>Deleted</button>
+        </div>
     </div>
         </div>
     )
