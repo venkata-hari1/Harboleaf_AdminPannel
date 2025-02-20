2e781a03307afedad64b6f960a8f280afa893419
Sanjeev <sanjeev.ksv@gmail.com>
2025-02-20 15:06:09 +0530

    action button hover

--- src/Components/ActionBtns/Actions.jsx
+++ src/Components/ActionBtns/Actions.jsx
@@ -1,11 +1,12 @@
 import './Styles.css'
 export default function Actions() {
     return (
-
+        
         <div className='action_btns_container'>
             <button className='gst-retrive-child'>Temporary Suspended</button>
             <button className='gst-retrive-child'>Suspended</button>
             <button className='gst-retrive-child'>Deleted</button>
         </div>
+        
     )
 }
